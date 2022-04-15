3034eeff3536a1087db44dde7cdcdcfadd7b80eb
nreis11 <nicholasreis@icloud.com>
2022-04-15 16:44:18 -0700

    Lecture 6

--- main.js
+++ main.js
@@ -3,13 +3,11 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 
 const animate = () => {
-  for (let i = 0; i < traffic.length; i++) {
-    traffic[i].update(road.borders, []);
-  }
+  traffic.forEach((car) => car.update(road.borders, []));
   car.update(road.borders, traffic);
   canvas.height = window.innerHeight;
 
@@ -17,9 +15,7 @@ const animate = () => {
   ctx.translate(0, -car.y + canvas.height * 0.7);
 
   road.draw(ctx);
-  for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(ctx, "red");
-  }
+  traffic.forEach((car) => car.draw(ctx, "red"));
   car.draw(ctx, "blue");
 
   ctx.restore();
